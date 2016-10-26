15f0c427e647a0ff3f449ff5ab94c2b00382a4c4
BradPickeringDunn <brad.pickering-dunn@adazzle.com>
2016-10-26 09:52:09 +0100

    Got the service running

--- Adazzle.Performace.Examples/Program.cs
+++ Adazzle.Performace.Examples/Program.cs
@@ -1,13 +1,5 @@
-﻿using Microsoft.Practices.Unity;
-using Adazzle.Common.Models;
-using OutTheBox.Notifications;
-using System.Web.Services.Description;
-using Adazzle.Campaign.Service.Api.Contracts;
-using Adazzle.Campaign.Service.Api.ServicesAdapter;
-
-namespace Adazzle.Performace.Examples
-{
-    class Program {
+﻿namespace Adazzle.Performace.Examples {
+   class Program {
       static void Main(string[] args) {
             
       }
